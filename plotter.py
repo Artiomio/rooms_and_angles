b0d7994b980b16cacb0d7eb9743c36ddca41f5c6
Artiom Peysahovsky <peysahovsky@gmail.com>
2024-01-16 05:02:58 +0300

    added sorting by one column

--- plotter.py
+++ plotter.py
@@ -5,13 +5,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-
 class Plot:
-    def __init__(self, column_oriented_json_path: str, 
-                 output_path: str=".",
-                 display=False,
-                 image_type: str='png'
-                 ):
+    def __init__(
+        self,
+        column_oriented_json_path: str,
+        output_path: str = ".",
+        display=True,
+        image_type: str = "png",
+    ):
         self.column_oriented_json_path = column_oriented_json_path
         self.df = pd.read_json(column_oriented_json_path, orient="column")
         self.columns = self.df.columns
@@ -21,22 +22,27 @@ class Plot:
         self.image_type = image_type
 
     def hist(self, field, *args, **kwargs):
-        """ Generate a histogram plot for the given field 
-        """
-        
+        """Generate a histogram plot for the given field"""
+
         self.df[field].hist(*args, **kwargs)
         img_fname = os.path.join(self.output_path, f"hist_{field}.{self.image_type}")
         plt.savefig(img_fname)
         self.output_filename_list.append(img_fname)
-    
+
         if not self.display_plots:
             plt.close()
         else:
             plt.show()
-        
 
-
-    def pair_plot(self, plot_type, col_1: str, col_2: str, *args, **kwargs):
+    def pair_plot(
+        self,
+        plot_type,
+        col_1: str,
+        col_2: str,
+        sort_by_first_column=True,
+        *args,
+        **kwargs,
+    ):
         if isinstance(plot_type, str):
             plot_func = getattr(plt, plot_type, None)
             plot_type_str = plot_type
@@ -48,20 +54,24 @@ class Plot:
         else:
             raise ValueError("plot_type must be a string or a callable function")
 
-        plot_func(self.df[col_1], self.df[col_2], *args, **kwargs)
-        img_fname = os.path.join(self.output_path, f"{plot_type_str}_{col_1}_{col_2}.{self.image_type}")
+        if sort_by_first_column:
+            sorted_by_first_column = sorted(zip(self.df[col_1], self.df[col_2]))
+            x, y = zip(*sorted_by_first_column)
+        else:
+            x, y = self.df[col_1], self.df[col_2]
+
+        plot_func(x, y, *args, **kwargs)
+        img_fname = os.path.join(
+            self.output_path, f"{plot_type_str}_{col_1}_{col_2}.{self.image_type}"
+        )
         plt.savefig(img_fname)
         self.output_filename_list.append(img_fname)
 
         if not self.display_plots:
             plt.close()
         else:
-            plt.show()    
-        
-        
-plot = Plot("./deviation.json")
-        
+            plt.show()
 
+
+plot = Plot("./deviation.json")
 plot.pair_plot(plt.scatter, "gt_corners", "rb_corners")
-plot.pair_plot(plt.scatter, "mean", "max")
-plot.hist('mean')
